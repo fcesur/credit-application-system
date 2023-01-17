0eb72ada06e3ada2563d23d8b2b2feaf1f73de1c
Fatih Cesur <fc.fatihcesur@gmail.com>
2023-01-17 03:31:00 +0300

    Initial commit

---
+++ src/test/java/com/fcesur/creditapplicationsystem/CreditApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcesur.creditapplicationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
